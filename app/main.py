58479e32dcbf508055f1b5e2272abe5891ad19bf
Cameron McCullough <mccullca1@gmail.com>
2026-02-13 14:53:46 -0600

    Add localhost:3003 to CORS origins

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:3002",
+        "http://localhost:3003",
     ],
     allow_credentials=True,
     allow_methods=["*"],
